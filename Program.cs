29edfc8291de896da5e6d530a8161abf27b9d359
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-07 15:22:00 -0300

    Exercicio Musica

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Exercicio_Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+            Console.ForegroundColor = ConsoleColor.Yellow;    
+            Console.WriteLine("Bk - Planos");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;    
+            Console.WriteLine("Eu arrumei toda a casa");
+            Console.WriteLine("Joguei fora as lembranças que não servem mais pra nada");
+            Console.WriteLine("Me livrei de toda sujeira");
+            Console.WriteLine("Aparecendo poeira, a gente dá uma espanada");
+            Console.WriteLine("Como é bom te olhar nos olhos");
+            Console.WriteLine("Planejar o futuro, planejar os nossos");
+            Console.WriteLine("Sem pensar nos próximos episódios");
+            Console.WriteLine("Vivemos num infinita-metragem, baby");
+            Console.WriteLine("Você fala pra eu nunca te esquecer, mas olha só pra você");
+            Console.WriteLine("Han, como me esquecer de você?");
+            Console.WriteLine("Não quero promessas, eu quero viver");
+            Console.WriteLine("E poder contar com você, a grana que nós tiver");
+            Console.WriteLine("Te olhando deitada na cama e o Sol adora seu corpo");
+            Console.WriteLine("Da janela do quarto eu me sinto iluminado, abençoado");
+            Console.WriteLine("Tirando fotos suas, o coração dividido entre a real e a do retrato, baby");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;    
+            Console.WriteLine("Tenho você por perto, nunca me senti tão completo");
+            Console.WriteLine("Nunca me senti tão seguro, perco o medo do fim do mundo");
+            Console.WriteLine("Tenho você por perto, me sinto caminhando certo");
+            Console.WriteLine("Eu que nem de tantos acertos, não errei em te ter no peito");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;    
+            Console.WriteLine("Você é tão gostosa que eu nem sofro");
+            Console.WriteLine("Em olhar você de costas indo embora");
+            Console.WriteLine("Eu amo o jeito que você se veste");
+            Console.WriteLine("Eu amo o jeito que tu tira o que tu veste, baby ");
+            Console.WriteLine("Baby, eu gosto tanto de ti");
+            Console.WriteLine("Mó sensação que eu não preciso mentir");
+            Console.WriteLine("É, talvez não menti tanto, nas suas pernas meditando");
+            Console.WriteLine("Esqueça o que eu falei sobre mentir, tava brincando");
+            Console.WriteLine("Podemos fazer todas as estrelas caírem   ");
+            Console.WriteLine("Com que todos se curvem, os que tão em volta se unirem");
+            Console.WriteLine("Em diante nada será como antes");
+            Console.WriteLine("Então vamos voar alto, vejo que tu cansou de rasante, com razão");
+            Console.WriteLine("Pra não se arrasar temos que ter sorte até em jogo de azar");
+            Console.WriteLine("Já que cansou desse ritmo uniforme");
+            Console.WriteLine("Então, na melhor forma, hoje vai trabalhar pra mim");
+            Console.WriteLine("Tu pelada é teu uniforme");
+            Console.WriteLine("Deixa a ambição fluir, não precisa medir terreno");
+            Console.WriteLine("Se queremos algo, qual o Mediterrâneo");
+            Console.WriteLine("Não importa os meios, temos todos os direitos");
+            Console.WriteLine("Quando a gente dá a mão temos o mundo nas mãos");
+            Console.WriteLine("Papo é de que é o melhor, esse é o fim das suas dúvidas");
+            Console.WriteLine("Papo é um mundo melhor, então só aceitar as dádivas");
+            Console.WriteLine("Ahn, ver você com sorriso igual das divas");
+            Console.WriteLine("Não tenha medo, essa vai ser a melhor das vidas, baby");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;    
+            Console.WriteLine("Tenho você por perto, nunca me senti tão completo");
+            Console.WriteLine("Nunca me senti tão seguro, perco o medo do fim do mundo");
+            Console.WriteLine("Tenho você por perto, me sinto caminhando certo");
+            Console.WriteLine("Eu que nem de tantos acertos, não errei em te ter no peito");
+            Console.WriteLine("");
+            Console.WriteLine("Tenho você por perto, nunca me senti tão completo");
+            Console.WriteLine("Nunca me senti tão seguro, perco o medo do fim do mundo");
+            Console.WriteLine("Tenho você por perto, me sinto caminhando certo");
+            Console.WriteLine("Eu que nem de tantos acertos, não errei em te ter no peito");
+            Console.WriteLine("");
+            Console.WriteLine("Tenho você por perto, nunca me senti tão completo");
+            Console.WriteLine("Nunca me senti tão seguro, perco o medo do fim do mundo");
+            Console.WriteLine("Tenho você por perto, me sinto caminhando certo");
+            Console.WriteLine("Eu que nem de tantos acertos, não errei em te ter no peito");
+            Console.ResetColor();
+        }
+    }
+}
